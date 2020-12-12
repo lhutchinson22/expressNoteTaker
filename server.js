9dcb430d6dd76a54a618101c1510c4a3c58d3b4e
lhutchinson22 <lbhutchinson022@gmail.com>
2020-12-11 17:36:01 -0800

    server set up and running

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
+
+// LISTENER
+// "starts" our server
+app.listen(PORT, () => {
+  console.log(`App listening http://localhost:${PORT}`);
+});
